9e89f0ba2b3ed280069dc3053bc979b3610e4461
Ravi Waghmare <raviwaghmare2908@gmail.com>
2020-02-18 16:46:39 +0530

    new changes

--- controllers/hardwareclassificationcontroller_controller.go
+++ controllers/hardwareclassificationcontroller_controller.go
@@ -59,7 +59,7 @@ func (r *HardwareClassificationControllerReconciler) Reconcile(req ctrl.Request)
 		return ctrl.Result{}, err
 	}
 
-	fmt.Printf("%+v", bmhHostList)
+	fmt.Printf("%+v", &bmhHostList)
 	// r.Log.Info("Fetched Baremetal host list successfully", "BareMetalHostList", bmhHostList)
 	// r.Log.Info("List", "BMH", bmhHostList)
 
