7ea1662137a9b6a711f784128a16776db8b8c3f0
Ravi Waghmare <raviwaghmare2908@gmail.com>
2020-02-19 16:59:34 +0530

    changes

--- validate/validation.go
+++ validate/validation.go
@@ -7,23 +7,33 @@ import (
 	bmh "github.com/metal3-io/baremetal-operator/pkg/apis/metal3/v1alpha1"
 )
 
-//
+//Comparison compare the host against the profile and filter the valid host
 func Comparison(hosts []bmh.BareMetalHost, profiles []hwcc.ExpectedHardwareConfiguration) {
 
-	fmt.Println("********************************")
-	fmt.Println("Validation wali file")
+	fmt.Println("Inside Comparison file")
 	for _, host := range hosts {
-		fmt.Printf("%+v", host.Status.HardwareDetails)
-		fmt.Println("")
-		fmt.Println("")
-	}
+		// fmt.Printf("%+v", host.Status.HardwareDetails)
+		fmt.Printf("CPU Count :- %+v \n", host.Status.HardwareDetails.CPU.Count)
+		fmt.Printf("Stoarage Count :- %+v \n", host.Status.HardwareDetails.Storage)
+		fmt.Printf("Stoarage Count :- %+v \n", host.Status.HardwareDetails.NIC)
+		fmt.Printf("Stoarage Count :- %+v \n", host.Status.HardwareDetails.RAMMebibytes)
 
-	// fmt.Println("*********************")
+		// for _, profile := range profiles {
+		// 	if profile.MinimumCPU.Count < host.Status.HardwareDetails.CPU.Count &&
+		// 		profile.MinimumDisk.SizeBytesGB < host.Status.HardwareDetails.Storage.SizeBytes*1024*1024 &&
+		// 		profile.MinimumNICS.NumberOfNICS < host.Status.HardwareDetails.NIC.NoOfNics &&
+		// 		profile.MinimumRAM < host.Status.HardwareDetails.RAMMebibytes {
+		// 		newHost, ok := validHost[host.Spec.HardwareDetails]
+		// 		if ok {
+		// 			validHost[host.Spec.HardwareDetails] = append(newHost, profile)
+		// 		} else {
+		// 			var validProfile []ironic.ExpectedHardwareConfiguration
+		// 			validHost[host.Spec.HardwareDetails] = append(validProfile, profile)
+		// 		}
 
-	for _, profile := range profiles {
-		fmt.Printf("%+v", profile)
-		fmt.Println("")
-		fmt.Println("")
+		// 	}
+		// }
+
 	}
 
 }
