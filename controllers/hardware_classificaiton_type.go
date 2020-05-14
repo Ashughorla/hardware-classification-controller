5cae6aac4411af8dbf25620059228467c33287d2
Ravi Waghmare <raviwaghmare2908@gmail.com>
2020-05-15 01:57:35 +0530

    updated the test cases

--- controllers/hardware_classificaiton_type.go
+++ controllers/hardware_classificaiton_type.go
@@ -3,12 +3,47 @@ package controllers
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 
+	hwcc "hardware-classification-controller/api/v1alpha1"
+
 	bmh "github.com/metal3-io/baremetal-operator/pkg/apis/metal3/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func getNamespace() string {
 	return "metal3"
+}
+
+func getExtractedHardwareProfile() []runtime.Object {
+	expectedHardwareClassification := hwcc.ExpectedHardwareConfiguration{
+		CPU: &hwcc.CPU{
+			MaximumCount: 1,
+			MinimumCount: 1,
+			MaximumSpeed: "1.2",
+			MinimumSpeed: "1",
+		},
+		Disk: &hwcc.Disk{
+			MaximumCount:            2,
+			MaximumIndividualSizeGB: 1000,
+			MinimumCount:            1,
+			MinimumIndividualSizeGB: 500,
+		},
+		NIC: &hwcc.NIC{
+			MaximumCount: 2,
+			MinimumCount: 1,
+		},
+		RAM: &hwcc.RAM{
+			MaximumSizeGB: 16,
+			MinimumSizeGB: 8,
+		},
+	}
+
+	expectedHardwareConfiguration := hwcc.HardwareClassification{
+		Spec: hwcc.HardwareClassificationSpec{
+			ExpectedHardwareConfiguration: expectedHardwareClassification,
+		},
+	}
+
+	return []runtime.Object{&expectedHardwareConfiguration}
 }
 
 func getHosts() []runtime.Object {
--- controllers/hardwareclassification_controller_test.go
+++ controllers/hardwareclassification_controller_test.go
@@ -10,8 +10,10 @@ import (
 
 	bmoapis "github.com/metal3-io/baremetal-operator/pkg/apis"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog/klogr"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 var _ = Describe("Hardware Classification Controller", func() {
@@ -30,11 +32,30 @@ var _ = Describe("Hardware Classification Controller", func() {
 		Expect(len(hostTest)).Should(Equal(len(result)))
 	})
 
+	It("Should check the reconcile function", func() {
+
+		config := getExtractedHardwareProfile()
+
+		c := fakeclient.NewFakeClientWithScheme(setupSchemeMm(), config...)
+		hardwareReconciler := &HardwareClassificationReconciler{
+			Client: c,
+			Log:    klogr.New(),
+		}
+		req := reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      "Hardware Classification Controller",
+				Namespace: getNamespace(),
+			},
+		}
+
+		res, err := hardwareReconciler.Reconcile(req)
+		fmt.Println("Reconcile output", res)
+		Expect(err).ShouldNot(Equal(nil))
+	})
+
 })
 
-//-----------------
-// Helper functions
-//-----------------
+//setupSchemeMm Add the bmoapi to our scheme
 func setupSchemeMm() *runtime.Scheme {
 	s := runtime.NewScheme()
 	if err := bmoapis.AddToScheme(s); err != nil {
