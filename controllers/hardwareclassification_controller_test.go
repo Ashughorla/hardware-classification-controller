c09443863c8a466815203d346ecb8f13407fe46f
Ravi Waghmare <raviwaghmare2908@gmail.com>
2020-05-19 17:53:47 +0530

    updated code

--- controllers/hardwareclassification_controller_test.go
+++ controllers/hardwareclassification_controller_test.go
@@ -93,7 +93,8 @@ var _ = Describe("Hardware Classification Controller", func() {
 				comparedHost := hcManager.MinMaxComparison(getTestProfileName(), validatedHardwareDetails, getExtractedHardwareProfile())
 				fmt.Println("Compared Host list", comparedHost)
 				if len(comparedHost) != 0 {
-					err := hcManager.SetLabel(context.TODO(), getObjectMeta(), comparedHost, BMHList, getObjectMeta().Labels)
+					err := hcManager.SetLabel(context.Background(), getObjectMeta(), comparedHost, BMHList, getObjectMeta().Labels)
+					fmt.Println("Label Update Error", err)
 					if err != nil {
 						fmt.Println("Label Set Successfully")
 					} else {
